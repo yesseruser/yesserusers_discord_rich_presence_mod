03556f23869d4ff71dc88fd9cfd0e104f34b4f8d
yesseruser <yesseruseryt@gmail.com>
2024-06-02 10:36:39 +0200

    Use deprecated RPC library instead of closed-source SDK

--- common/src/main/java/cz/yesseruser/discordrpc/ExampleMod.java
+++ common/src/main/java/cz/yesseruser/discordrpc/ExampleMod.java
@@ -1,8 +1,9 @@
 package cz.yesseruser.discordrpc;
 
+import club.minnced.discord.rpc.DiscordEventHandlers;
+import club.minnced.discord.rpc.DiscordRPC;
+import club.minnced.discord.rpc.DiscordRichPresence;
 import com.mojang.logging.LogUtils;
-import de.jcm.discordgamesdk.Core;
-import de.jcm.discordgamesdk.CreateParams;
 import dev.architectury.event.events.client.ClientTickEvent;
 import net.minecraft.client.MinecraftClient;
 import org.slf4j.Logger;
@@ -12,28 +13,26 @@ public final class ExampleMod {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     // L is not part of the ID, it states the number is of type long.
-    public static final long DISCORD_CLIENT_ID = 1246435446838399037L;
+    public static final String DISCORD_CLIENT_ID = "1246435446838399037";
+
+    public static final DiscordRPC RPC_LIB = DiscordRPC.INSTANCE;
+    public static final long START_TIMESTAMP = System.currentTimeMillis() / 1000;
 
     public static void init() {
-        LOGGER.info("Creating params");
-        /*try (*/CreateParams discordParams = new CreateParams();//) {
-            LOGGER.info("Setting client ID");
-            discordParams.setClientID(DISCORD_CLIENT_ID);
-            LOGGER.info("Client ID set");
-            discordParams.setFlags(CreateParams.getDefaultFlags());
-            LOGGER.info("Flags set.");
-
-            try (Core core = new Core(discordParams)) {
-
-            }
-        //}
-
-        discordParams.close();
-
+        RPC_LIB.Discord_Initialize(DISCORD_CLIENT_ID, new DiscordEventHandlers(), true, "");
 
         // Write common init code here.
         ClientTickEvent.CLIENT_POST.register((MinecraftClient client) -> {
             LOGGER.info("Tick");
+
+            DiscordRichPresence presence = new DiscordRichPresence();
+            presence.startTimestamp = START_TIMESTAMP;
+
+            presence.details = "Testing...";
+            // TODO: Add presence logic here...
+
+            RPC_LIB.Discord_UpdatePresence(presence);
+            RPC_LIB.Discord_RunCallbacks();
         });
     }
 }
